dcef32033106eb4537a356ce629f4d91e82950e4
vhgauto <victor.gauto@outlook.com>
2024-09-14 23:43:13 -0300

    agrego Corrientes

--- scripts/Corrientes/script.R
+++ scripts/Corrientes/script.R
@@ -4,10 +4,6 @@ zoom <- 9
 
 obj <- f_rayshader(provincia, zoom)
 
-linewidth1 <- 10
-linewidth2 <- 4
-x_lty <- 1
-
 # ventana para previsualizar
 obj$matrix |>
   height_shade(
@@ -15,37 +11,14 @@ obj$matrix |>
       c(
         "#031D2C", "#2E77AB", "#D1B252", "#462C0B"
       ), bias = 1
-    )(128)
+    )(1024)
   ) |>
-  # departamentos
-  add_overlay(
-    generate_line_overlay(
-      geometry = obj$dpto,
-      extent = obj$dem,
-      heightmap = obj$matrix,
-      color = "black",
-      linewidth = linewidth1,
-      lty = 1
-    ),
-    alphalayer = .9
-  ) |>
-  add_overlay(
-    generate_line_overlay(
-      geometry = obj$dpto,
-      extent = obj$dem,
-      heightmap = obj$matrix,
-      color = "white",
-      linewidth = linewidth2,
-      lty = 1
-    ),
-    alphalayer = .9
-  )|>
   # mapa
   plot_3d(
     heightmap = obj$matrix,
     background = "white",
     windowsize = c(600, 600*obj$asp),
-    zscale = 2, # 5
+    zscale = 2,
     solid = FALSE,
     shadow = TRUE,
     shadow_darkness = 1
@@ -68,7 +41,7 @@ file_alto <- round(file_ancho*obj$asp)
     filename = file_name,
     preview = TRUE,
     light = FALSE,
-    environment_light = hdri_file,
+    environment_light = hdri_file(),
     intensity_env = 1,
     interactive = FALSE,
     width = file_ancho,
@@ -84,15 +57,20 @@ file_alto <- round(file_ancho*obj$asp)
   beepr::beep(sound = 2)
 }
 
+# 47m
+
+# abro figura
 browseURL(file_name)
 
+# cierro ventana interactiva
 rgl::close3d()
 
-# anotación ---------------------------------------------------------------
+# anotaciones -------------------------------------------------------------
 
 # leo la imagen generada
-img <- image_read(file_name)
+img <- image_read(f_actual(provincia, zoom))
 
+# genero caption
 f_caption(
   color1 = "#462C0B", # vhgauto
   color2 = "#2E77AB", # RR.SS.
@@ -106,27 +84,35 @@ f_simbolos(provincia)
 img |>
   # título
   image_annotate(
-    text = provincia,
+    text = "Corrientes",
     color = "#74ACDF",
     location = "+200+400",
     size = 400,
     font = "Cambria",
-    gravity = "southeast") |>
+    gravity = "southeast"
+  ) |>
   # escudo
   image_composite(
     composite_image = image_scale(escudo, "x750"),
     gravity = "southeast",
-    offset = "+200+900") |>
+    offset = "+200+900"
+  ) |>
   # bandera
   image_composite(
     composite_image = image_scale(bandera, "1000x"),
     gravity = "northwest",
-    offset = "+100+100") |>
+    offset = "+100+100"
+  ) |>
   # autor
   image_composite(
     composite_image = image_scale(autor, "2000x"),
     gravity = "southwest",
-    offset = "+100+100") |>
+    offset = "+100+100"
+  ) |>
   # guardo
   image_write(
-    path = f_nombre(provincia, zoom))
+    path = f_nombre(provincia, zoom)
+  )
+
+# reduzco tamaño
+f_imagen(provincia)
