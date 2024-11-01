62c0dfd8682c6ff83d0d4c8e169deccefed7d3f6
Víctor Gauto <victor.gauto@outlook.com>
2024-10-31 23:40:50 -0300

    agrego paleta de colores al nombre de la región en sitio web

--- scripts_quarto/mapa.R
+++ scripts_quarto/mapa.R
@@ -2,26 +2,45 @@
 # paquetes y colores
 source("scripts_quarto/soporte_quarto.R")
 
+# paletas de colores de cada región
+source("scripts_quarto/_paletas.R")
+
 # tema de la figura
 theme_set(theme_void())
 
 # función que genera los mapas de cada región
 f_gg <- function(datos, expand = TRUE) {
+
+  df <- inner_join(
+    datos,
+    label_color_tbl,
+    by = join_by(region)
+  ) |>
+    mutate(
+      ll = glue(
+        "<span style='font-family: Bebas Neue; color: {c3}; font-size:50px'>",
+        "{region}<br>{label_color}"
+      )
+    )
+
   ggplot() +
     geom_sf(
-      data = datos, fill = c4, color = c1, linewidth = .1
+      data = df, fill = c4, color = c1, linewidth = .1
     ) +
     geom_sf_interactive(
-      data = datos,
-      aes(data_id = region, onclick = glue("window.open(\"{link}\")")),
-      linewidth = .1,
-      color = c1, fill = c2, hover_nearest = TRUE
+      data = df,
+      aes(
+        data_id = region, onclick = glue("window.open(\"{link}\")"),
+        tooltip = ll
+      ),
+      linewidth = .1, tooltip_fill = "transparent", color = c1, fill = c2,
+      hover_nearest = FALSE
     ) +
     coord_sf(expand = expand, clip = "off")
 }
 
 # función que lee cada vector
-f_vector <- function(vector) {
+f_vector <- function(vector, tolerancia = 0) {
   st_read(glue("vectores/{vector}"), quiet = TRUE) |>
     mutate(
       region2 = str_replace_all(region, " ", "_")
@@ -34,26 +53,19 @@ f_vector <- function(vector) {
         "https://raw.githubusercontent.com/vhgauto/arg_rayshader/refs/heads/",
         "main/figuras/{region}/{region2}.png"
       )
-    )
+    ) |>
+    st_simplify(dTolerance = tolerancia)
 }
-
-# arg -> Argentina continental, Islas Malvinas
-# caba -> Ciudad Autónoma de Buenos Aires
-# ig -> Islas Georgias del Sur
-# im -> Islas Malvinas
-# is -> Islas Sandwich del Sur
 
 # leo cada vector
 # el vector de Argentina tengo que simplificarlo para que aparezca el
 # mapa animado en quarto
-arg_sf <- f_vector("arg_cont.gpkg") |>
-  st_simplify(dTolerance = 1000)
-caba_sf <- f_vector("arg_caba.gpkg")
-ig_sf <- f_vector("arg_ig.gpkg")
-im_sf <- f_vector("arg_im.gpkg")
+arg_sf <- f_vector("arg_cont.gpkg", 100)
+caba_sf <- f_vector("arg_caba.gpkg", 100)
+ig_sf <- f_vector("arg_ig.gpkg", 100)
+im_sf <- f_vector("arg_im.gpkg", 100)
 is_sf <- f_vector("arg_is.gpkg")
-aa_sf <- f_vector("arg_aa.gpkg") |>
-  st_simplify(dTolerance = 2000)
+aa_sf <- f_vector("arg_aa.gpkg")
 
 # genero los mapas de cada región
 g_arg <- f_gg(arg_sf, expand = FALSE)
@@ -83,18 +95,17 @@ g_region <- ggplot() +
 # acomodo todas las partes
 diseño <- "
 ####
-ABBB
 A###
-ACDG
-AEFG
+ABDF
+ACEF
 "
 
 # figura final
-g <- g_arg + g_region + g_caba + g_im + g_ig + g_is + g_aa +
+g <- g_arg + g_caba + g_im + g_ig + g_is + g_aa +
   plot_layout(
     design = diseño,
-    widths = c(1, .5, .5, .5),
-    heights = c(1, 5, 5, 5, 5, 5)
+    widths = c(1, .5, .5, .5, 5),
+    heights = c(1, 5, 5, 5, 5)
   )
 
 # figura interactiva
@@ -102,13 +113,13 @@ g_int <- girafe(
   ggobj = g,
   bg = "transparent",
   options = list(
-    opts_hover(
-      css = girafe_css(
-        css = glue("fill:{c3}"),
-        text = glue(
-          "stroke:none;fill:{c3};fill-opacity:1;font-family:Bebas Neue"
-        )
-      )
+    opts_tooltip(
+      css = "font-size: 50px; line-height: 70px;",
+      use_fill = TRUE,
+      offx = 400,
+      offy = 20,
+      opacity = 1,
+      use_cursor_pos = FALSE
     ),
     opts_sizing(width = 1, rescale = TRUE),
     opts_toolbar(saveaspng = FALSE)
