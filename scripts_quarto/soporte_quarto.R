1ef8092ba69741909d887cf1c9fa0ffe9a81d896
Víctor Gauto <victor.gauto@outlook.com>
2024-11-01 01:20:31 -0300

    agrego arcoiris al final del sitio web como bloques de color

--- scripts_quarto/soporte_quarto.R
+++ scripts_quarto/soporte_quarto.R
@@ -13,3 +13,20 @@ c2 <- "#75AEE0"
 c3 <- "#F6B606"
 c4 <- "white"
 c5 <- "#666666"
+
+# punto de separador
+separador <- glue("<span style='color: {c3};'>·</span>")
+
+# espacios para las RRSS
+espacios <- "&nbsp;&nbsp;&nbsp;&nbsp;"
+
+# arcoiris al final del sitio web
+
+# cantidad de colores y cuadrado
+n_col <- 200
+cuadrado <- "■" #"█"
+
+arcoiris <- glue(
+  "<span style='color: {rainbow(n_col)}; font-size: 10px'>{cuadrado}</span>"
+) |>
+  str_flatten()
